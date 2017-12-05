ca18bf0131db11a29248d5e6be3b3786c51c93c6
Kevin von Horn <vonhorn.kevin@gmail.com>
2017-12-05 09:51:22 +0000

    Finish get_live_info.rb

--- util/get_live_info.rb
+++ util/get_live_info.rb
@@ -49,10 +49,28 @@ if not (options[:uid] and options[:pw])
   end
 end
 
+# Note: These are some poker room names you can use; check the home page for more
+# Set the default poker_room_name
+#poker_room_name = 'Ameristar Black Hawk'
+#poker_room_name = 'Atlantis Casino'
+#poker_room_name = 'Boulder Station'
+#poker_room_name = 'Caesars Palace'
+#poker_room_name = 'Capitol Casino'
+#poker_room_name = 'Commerce Casino'
+#poker_room_name = 'Flamingo Las Vegas'
+#poker_room_name = "Harvey's Lake Tahoe"
+#poker_room_name = 'Peppermill Casino'
+#poker_room_name = 'Planet Hollywood Las Vegas'
+#poker_room_name = 'Red Rock Casino'
+#poker_room_name = 'Silver Legacy'
+#poker_room_name = 'The Orleans'
+poker_room_name = 'Thunder Valley'
+
 
 # TODO: Get these from the environment?
 uid = options[:uid]
 pw = options[:pw]
+poker_room_name = options[:room] if options[:room]
 
 
 # Set up Chrome
@@ -67,7 +85,10 @@ capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
 
 @driver_quit_called = false
 at_exit do
-  @driver.quit unless @driver.nil? or @driver_quit_called
+  unless @driver.nil? or @driver_quit_called
+    #@driver.save_screenshot 'at_exit.png'
+    @driver.quit
+  end
 end
 
 
@@ -78,30 +99,25 @@ end
 home_page = VenuesPage.new(@driver)
 @logger.info "Getting home page"
 home_page.get
-#@logger.info "Current URL: #{@driver.current_url}"
+@logger.info "Current URL: #{@driver.current_url}"
 
 
-# TODO: check to see if logged in. Log in if not
+# Log in if necessary
 logged_in = home_page.logged_in?
-#@logger.debug "Logged in? #{logged_in}"
-home_page.login_as(uid, pw)
-#require 'irb'; binding.irb
-#@driver.save_screenshot 'post_login.png'
-@driver.close; @driver.quit; @driver_quit_called = true; puts 'Sleeping 10'; sleep 10; exit 1
-# TODO: Find the link on the page like poker_room_name and click it
-# TODO: Scrape the live info
+if not logged_in
+  @logger.debug "Logging in as #{uid}"
+  home_page.login_as(uid, pw)
+end
 
-# Load the poker room page
-##poker_room_name = 'red-hawk-casino-placerville'
-#poker_room_name = 'bicycle-casino-bell-gardens'
-#poker_room_page = PokerRoomPage.new @driver, "/poker-room/#{poker_room_name}"
-#poker_room_page.get
-#
-#
-## Get all the data
-#live_info = poker_room_page.get_live_info
-#puts JSON.pretty_generate(live_info)
-#
-#
-#@driver.quit
-#@driver_quit_called = true
+
+# Find the link on the page containing the text in poker_room_name and click it
+poker_room_page = home_page.click_casino_name(poker_room_name)
+
+
+# Scrape the live info
+live_info = poker_room_page.get_live_info
+puts JSON.pretty_generate(live_info)
+
+
+@driver.quit
+@driver_quit_called = true
